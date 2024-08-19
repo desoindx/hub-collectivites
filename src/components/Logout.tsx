ef73a97c08a86bd6aef42215d23fccdd8d7f8160
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2024-08-19 15:07:52 +0200

    feat: logout from AgentConnect

--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -4,11 +4,19 @@ import Button from "@codegouvfr/react-dsfr/Button";
 import { signOut } from "next-auth/react";
 import React from "react";
 import styles from "./Logout.module.css";
+import { useRouter } from "next/navigation";
+import { ROUTES } from "@/app/routes";
 
 const Logout = () => {
+
+  const router = useRouter();
+
+  const onLogout = async () => {
+    router.push(ROUTES.DECONNEXION);
+  };
   return (
-    <Button className={styles.logout} onClick={() => signOut()}>
-      Se deconnecter
+    <Button className={styles.logout} onClick={() => onLogout()}>
+      Se déconnecter
     </Button>
   );
 };
