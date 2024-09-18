253efdc68d6d7be9cbd63a75a1089d361ae4e5be
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2024-09-18 10:37:59 +0200

    style: some ui changes on project page

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -8,9 +8,9 @@ import Box from "./Box";
 import classNames from "classnames";
 import Service from "./Service";
 import { ROUTES } from "@/app/routes";
-import { OptionBadge } from "./SearchableSelect/OptionBadge";
 import { thematiquesLabel } from "@/services/thematiques";
 import { statusLabel } from "@/services/status";
+import Accordion from "@codegouvfr/react-dsfr/Accordion";
 
 const Project = ({ baseProject, services }: { baseProject: ProjectType; services: ServiceType[] }) => {
   return (
@@ -30,52 +30,58 @@ const Project = ({ baseProject, services }: { baseProject: ProjectType; services
         ]}
       />
       <h1>{baseProject.name}</h1>
-      <Box
-        header={
-          <>
-            <h2 className={styles.title}>Description</h2>
-            <Button className={styles.cta} iconId="fr-icon-edit-line" priority="tertiary no outline">
-              Editer
-            </Button>
-          </>
-        }
-      >
-        {baseProject.description}
-      </Box>
-      <Box
-        header={
-          <>
-            <h2 className={styles.title}>Statut</h2>
-          </>
-        }
-      >
-        {statusLabel[baseProject.status]}
-      </Box>
-      <Box
-        header={
-          <>
-            <h2 className={styles.title}>Thématiques</h2>
-            <Button className={styles.cta} iconId="fr-icon-edit-line" priority="tertiary no outline">
-              Editer
-            </Button>
-          </>
-        }
-      >
-        <div className={styles.thematiques}>
-          {baseProject.thematiques.map((thematique) => (
-            <div key={thematique} className={classNames("fr-tag", styles.thematique)}>
-              {thematiquesLabel[thematique]}
-            </div>
-          ))}
-        </div>
-      </Box>
+      <Accordion label="Voir plus" className={styles.accordion}>
+        <Box
+          header={
+            <>
+              <h2 className={styles.title}>Description</h2>
+              <Button className={styles.cta} iconId="fr-icon-edit-line" priority="tertiary no outline">
+                Editer
+              </Button>
+            </>
+          }
+        >
+          {baseProject.description}
+        </Box>
+        <Box
+          header={
+            <>
+              <h2 className={styles.title}>Statut</h2>
+            </>
+          }
+        >
+          {statusLabel[baseProject.status]}
+        </Box>
+        <Box
+          header={
+            <>
+              <h2 className={styles.title}>Thématiques</h2>
+              <Button className={styles.cta} iconId="fr-icon-edit-line" priority="tertiary no outline">
+                Editer
+              </Button>
+            </>
+          }
+        >
+          <div className={styles.thematiques}>
+            {baseProject.thematiques.map((thematique) => (
+              <div key={thematique} className={classNames("fr-tag", styles.thematique)}>
+                {thematiquesLabel[thematique]}
+              </div>
+            ))}
+          </div>
+        </Box>{" "}
+      </Accordion>
+
       <Box
         header={
           <>
             <div>
-              <h2>Services aidants</h2>
-              <span className={classNames("fr-text--sm", styles.subtitle)}>
-                Les services de l’écosystème qui répondent à votre problématique
+              <div className={classNames("fr-text--bold", styles.title, styles.titleServices)}>
+                Services numériques en lien avec votre projet
+              </div>
+              <span className={classNames("fr-text--sm", styles.subtitleServices)}>
+                Ces services sont en lien avec les <strong>thématiques, l’état d’avancement</strong> ainsi que la{" "}
+                <strong>localisation</strong> de votre projet. En savoir plus
               </span>
             </div>
             <div className={styles.tag}>
